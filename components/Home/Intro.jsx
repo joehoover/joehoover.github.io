7ad92bf046b0458521ac939783ed076fd0d9aa7d
Joe Hoover <joe@replicate.com>
2024-01-31 07:58:04 -0500

    Update styles and links in components

--- components/Home/Intro.jsx
+++ components/Home/Intro.jsx
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h4 className="md:text-left text-xl mt-5 md:pl-8 md:pl-8 font-bold">
+      <h4 className="md:text-left text-4xl mt-5 md:pl-8 md:pl-8">
         Notes from the manifold
       </h4>
     </section>
